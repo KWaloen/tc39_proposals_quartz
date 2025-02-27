6f250c45ebd912fac373a2fa0e5ef1027726ea4e
KWaloen <k.waloen91@hotmail.com>
2025-02-27 11:31:37 +0100

    reduce scale for global graph

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -47,7 +47,7 @@ export const defaultContentPageLayout: PageLayout = {
         drag: true,
         zoom: true,
         depth: -1,
-        scale: 0.9,
+        scale: 0.5,
         repelForce: 0.5,
         centerForce: 0.3,
         linkDistance: 70,
